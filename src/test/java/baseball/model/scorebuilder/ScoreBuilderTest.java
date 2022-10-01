82ac1133156c2fdda123f3cc32c582ed26812169
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-01 22:43:53 +0900

    refactor: [#63] ParameterizedTest, RepeatedTest 에서 테스트 케이스 세부 정보 보여주기
    ParameterizedTest, RepeatedTest 어노테이션에서
    name 필드를 사용하면 테스트 실행 결과에서 세부 정보를 드러낼 수 있다
    
    관련 일감 링크
    [ParameterizedTest, RepeatedTest 에서 테스트 케이스 세부 정보 보여주기](https://github.com/OptimistLabyrinth/java-baseball-precourse/issues/63)

--- src/test/java/baseball/model/scorebuilder/ScoreBuilderTest.java
+++ src/test/java/baseball/model/scorebuilder/ScoreBuilderTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreBuilderTest {
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}, {1}, 점수는 {2}-{3}")
     @DisplayName("세 자리 숫자 입력에 대해서 올바른 strike, ball 을 반환해야 한다")
     @CsvSource(value = {"123:123:3:0", "123:124:2:0", "123:135:1:1", "123:891:0:1", "123:312:0:3"}, delimiter = ':')
     public void shouldGetCorrectStrikeAdnBall(
